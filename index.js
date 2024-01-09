747b7ee26f9500934ff5312d473a19605d4c8621
Jayant-001 <jayant.gupta.dln@gmail.com>
2024-01-10 01:19:17 +0530

    ejs layout set

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path")
+const ejs = require('ejs')
+var expressLayouts = require('express-ejs-layouts');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+
+// Set the layout directory
+app.use(expressLayouts)
+app.set("views", path.resolve("./resources/views"));
+// app.set("views", path.join(__dirname, "./resources/views"))
+app.set('view engine', 'ejs');
+app.set("layout", false);
+
+
+app.get('/', (req, res) => {
+      res.render('home');
+})
+
+app.get('/home', (req, res) => {
+    res.render('home')
+})
+
+app.listen(PORT, () => {
+    console.log("Server is listening on PORT", PORT);
+});
